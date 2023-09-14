6bebd3f664a9cb3b9b77c055bcbc3e0077d6c770
kat <sphhhsh@gmail.com>
2023-09-14 14:02:55 -0300

    save

--- srcs/requirements/mariadb/conf/init_mariadb.sh
+++ srcs/requirements/mariadb/conf/init_mariadb.sh
@@ -1,13 +1,11 @@
 #!bin/bash
 
-service mariadb start
-mariadb -e "CREATE DATABASE db;"
-
-#chown -R mysql:mysql /var/lib/mysql/db
-
-mysqladmin status
-
 # CONFIGURE DB USERS
-mariadb -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-service mariadb stop
+mariadb << EOF
+CREATE DATABASE wordpressdb;
+CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+EOF
+
+
+chown -R mysql:mysql /var/lib/mysql/wordpressdb
