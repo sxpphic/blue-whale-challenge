b42ad94f68440493a9911efaeb6218ca7d64cf62
kat <sphhhsh@gmail.com>
2023-09-17 12:09:41 -0300

    feat(nginx, wordpress): add new conf files

---
+++ srcs/requirements/nginx/conf/wp_conf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p /var/www/html/$DOMAIN_NAME
+cd /var/www/html/$DOMAIN_NAME
+wget https://wordpress.org/latest.tar.gz
+tar -xvzf latest.tar.gz
+rm latest.tar.gz
+
+useradd nginx
+groupadd nginx
+chown -R nginx /var/www/html/$DOMAIN_NAME/
+
