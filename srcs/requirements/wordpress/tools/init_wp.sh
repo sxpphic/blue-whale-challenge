9c9f2f3a494c5840a988fead472df8c17bc2928d
kat <sphhhsh@gmail.com>
2023-09-22 15:46:53 -0300

    tentando outras coisas, ainda n ta indo

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -4,16 +4,18 @@ chmod 755 wordpress
 
 #cd /var/www/html/$DOMAIN_NAME
 
-cp -R wordpress /var/www/html/$DOMAIN_NAME/
 
-cp /var/www/html/$DOMAIN_NAME/wordpress/wp-config-sample.php /var/www/html/$DOMAIN_NAME/wordpress/wp-config.php
+# tenatar com mv
+cp -R wordpress/* /var/www/html/
+
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
 useradd nginx
-chown -R nginx /var/www/html/$DOMAIN_NAME/
+chown -R nginx /var/www/html/
 
-sed -i "s/database_name_here/wordpressdb/g" /var/www/html/$DOMAIN_NAME/wordpress/wp-config.php
-sed -i "s/username_here/$MYSQL_USER/g" /var/www/html/$DOMAIN_NAME/wordpress/wp-config.php
-sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/html/$DOMAIN_NAME/wordpress/wp-config.php
+sed -i "s/database_name_here/wordpressdb/g" /var/www/html/wp-config.php
+sed -i "s/username_here/$MYSQL_USER/g" /var/www/html/wp-config.php
+sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/html/wp-config.php
 
 #sed -i "s/localhost/$DB_HOST/g" wp-config.php
 
