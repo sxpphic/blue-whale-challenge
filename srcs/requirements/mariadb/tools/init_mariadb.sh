d6805345795e191db79daf196d74eceebf31d77e
sxpphickat <sphhhsh@gmail.com>
2023-10-15 16:43:58 -0300

    tentando coisas novas

--- srcs/requirements/mariadb/tools/init_mariadb.sh
+++ srcs/requirements/mariadb/tools/init_mariadb.sh
@@ -5,19 +5,18 @@ chown -R mysql:mysql /var/lib/mysql
 service mariadb restart
 
 mariadb << EOF
-CREATE DATABASE IF NOT EXISTS wordpressdb CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON wordpressdb.* TO '$MYSQL_USER'@'localhost';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'localhost';
 
 CREATE USER IF NOT EXISTS 'lilith'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 GRANT ALL PRIVILEGES ON *.* to 'lilith'@'localhost' WITH GRANT OPTION;
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;
-
 FLUSH PRIVILEGES;
 EOF
 
+#ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;
 kill  $(cat /var/run/mysqld/mysqld.pid)
 
 mariadbd
