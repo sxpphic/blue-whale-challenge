407f8d9556c37f11300b3132a3377629fbc7a935
sxpphickat <sphhhsh@gmail.com>
2023-11-13 16:26:02 -0300

    echo mod

--- srcs/requirements/nginx/tools/init_nginx.sh
+++ srcs/requirements/nginx/tools/init_nginx.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTS_ -subj "/C=BR/L=BR/O=42/OU=student/CN=vipereir.42.fr"
-																										# certs é opath
-#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=BR/L=BR/O=42/OU=student/CN=[vipereir.42.fr](http://vipereir.42.fr/)"
 
 echo "
 server {
@@ -12,21 +10,19 @@ server {
     #server_name www.$DOMAIN_NAME $DOMAIN_NAME;
 
     ssl_certificate $CERTS_;
-    ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;" > /etc/nginx/sites-available/default
+    ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
 
-
-echo '
     ssl_protocols TLSv1.3;
 
     index index.php;
     root /var/www/html;
 
     location ~ [^/]\.php(/|$) { 
-            try_files $uri =404;
+            try_files \$uri =404;
             fastcgi_pass wordpress:9000;
             include fastcgi_params;
-            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         }
-} ' >>  /etc/nginx/sites-available/default
+} " > /etc/nginx/sites-available/default
 
 nginx -g "daemon off;"
